2bd280bf3168d39dc662f9551d7a35747c6fa621
Fotis Gimian <fgimiansoftware@gmail.com>
2016-03-20 00:53:52 +1100

    Updated the plistinfo script to output YAML instead of a dict

--- plistinfo.py
+++ plistinfo.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 from pprint import pprint
 import os
 from glob import glob
 import sys
+import yaml
 
 import biplist
 
@@ -11,7 +14,7 @@ def main():
     try:
         dest = sys.argv[1]
     except IndexError:
-        print 'Usage: {} [filename]'.format(__file__)
+        print('Usage: {} [filename]'.format(__file__))
         exit(1)
 
     if not dest.startswith('/') and not dest.startswith('~'):
@@ -34,15 +37,15 @@ def main():
                     plist_file = appstore_plist_file[0]
 
         dest = plist_file
-
     else:
-
         dest = os.path.expanduser(dest)
 
     try:
-        pprint(biplist.readPlist(dest), indent=2)
+        plist_data = biplist.readPlist(dest)
+        print(yaml.dump(plist_data, default_flow_style=False), end='')
+        # pprint(plist_data, indent=2)
     except IOError:
-        print 'ERROR: file not found'
+        print('ERROR: file not found')
         exit(1)
 
 
