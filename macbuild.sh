538918254f1552b22ff60ebf8a27d9c352b6aa61
Fotis Gimian <fgimiansoftware@gmail.com>
2016-12-30 20:11:33 +1100

    Added some colours and a header to the main macbuild shell script

--- macbuild.sh
+++ macbuild.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
+# Colours
+BOLD='\033[1m'
+RED='\033[91m'
+GREEN='\033[92m'
+YELLOW='\033[93m'
+BLUE='\033[94m'
+ENDC='\033[0m'
+
 # Prompt the user for their sudo password
 sudo -v
 
 # Enable passwordless sudo for the macbuild run
 sudo sed -i -e "s/^%admin.*/%admin  ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
 
+# Display a pretty header
+echo
+echo -e "${BOLD}Mac Build (using Ansible)${ENDC}"
+echo
+
 # Install Homebrew
 if ! which brew > /dev/null 2>&1
 then
-    echo "Installing Homebrew"
+    echo -e "${BLUE}Installing Homebrew${ENDC}"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
 fi
 
 # Install Python
 if ! brew list python3 > /dev/null 2>&1
 then
-    echo "Installing Python 3.x"
+    echo -e "${BLUE}Installing Python 3.x${ENDC}"
     brew install python3
 fi
 
 # Install Ansible (using pip is the officially supported way)
 if ! pip3 show ansible > /dev/null 2>&1
 then
-    echo "Installing Ansible"
+    echo -e "${BLUE}Installing Ansible${ENDC}"
     # TODO: switch back to pip when Ansible 2.2.1.0 is released
     # pip3 install ansible
     pip3 install git+git://github.com/ansible/ansible.git@stable-2.2
@@ -40,9 +53,9 @@ fi
 
 if [ ! -z "$HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR" ]
 then
-  echo "Using cask music software basedir of ${HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR}"
+  echo -e "${GREEN}Using cask music software basedir of ${HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR}${ENDC}"
 else
-  echo "Unable to find the music software basedir"
+  echo -e "${RED}Unable to find the music software basedir${ENDC}"
 fi
 
 # Perform the build
